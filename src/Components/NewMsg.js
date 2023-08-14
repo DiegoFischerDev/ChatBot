96fade97560e945f7daa94be542df18676a6ea88
DiegoFischerDev <diegofischer.dev@gmail.com>
2023-08-14 19:33:06 +0100

    try

--- src/Components/NewMsg.js
+++ src/Components/NewMsg.js
@@ -10,29 +10,17 @@ export default function NewMsg({ userImg, msgType, msg }) {
 
   const [Animation, setAnimation] = useState(msgType === "user" ? false : true)
   const [botImg, setBotImg] = useState(robotThinkingImg)
-  const [directory, setDirectory] = useState("Images/robotThinking.gif")
 
-  useEffect(()=>{
-    
-    if(msgType === "user"){
-      setDirectory("Images/userImg.jpg")
-    }
-    if(msgType === "bot"){
-      if(botImg === robotThinkingImg){
-        setDirectory("Images/robotThinking.gif")
-      } else {
-        setDirectory("Images/robotOk.gif")
-      }
-    }
-  },[botImg])
 
   return (
     <div className='w-100% min-h-[20px] p-3 rounded-md flex mb-3 items-center'>
 
-      <img
-      src={directory}
-      alt="Profile Picture"
-      className="rounded-full w-[15%]"
+      <Image
+        src={msgType === "user" ? userImg : botImg}
+        alt="Profile Picture"
+        className="rounded-full"
+        width={80}
+        height={80}
       />
 
       <div className='w-[82%] ml-4 mobile:text-sm'>
